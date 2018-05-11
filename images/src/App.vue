30d08184626caa5c24a3af044fecbd2e022a0a79
Guilherme Leonardo Utzig <guilherme.utzig@maqina.com.br>
2018-05-11 10:41:24 -0300

    Projeto de upload de imagens iniciado

---
+++ images/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style lang="scss" src="./App.scss"></style>
---
+++ images/src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a class="active item">
+      Image Storage
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Log in
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
